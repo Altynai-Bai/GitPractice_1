f7ff89815870b4260880b08cdb693e9b150bc448
Altynai-Bai <harmoni5413@gmail.com>
2021-07-29 13:33:28 -0500

    creating ArrayList

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,5 +1,11 @@
 package day3;
 
+import java.util.ArrayList;
+
 public class ListPractice {
 
+
+    public static void main(String[] args) {
+        ArrayList<String> lst = new ArrayList<>();
+    }
 }
